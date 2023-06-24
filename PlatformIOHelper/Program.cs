25ae41f9cd62b2c85370abefdb19377c0bca3a13
DSikes <david@sikespc.com>
2023-06-24 10:40:26 -0400

    Initial commit

---
+++ PlatformIOHelper/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PlatformIOHelper;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine();
+        Console.WriteLine("----------========== PlatformIO Tools ==========----------");
+        Console.WriteLine("Version: 0.1");
+        Console.WriteLine();
+        Console.WriteLine("[1] - Upload via USB then prompt to open serial monitor");
+        Console.WriteLine("[2] - Upload via USB then prompt to open serial monitor");
+        Console.WriteLine();
+        Console.Write("Please press a key to make a selection: ");
+        
+        var userKeyResponse = Console.ReadKey();
+
+        Console.WriteLine();
+        Console.WriteLine("User responded: " + userKeyResponse);
+    }    
+}
